95c24c56524b09a1b1a4c8f1ef2b57ca6afe0eca
MrOrangeUS <71477247+MrOrangeUS@users.noreply.github.com>
2025-07-01 07:55:48 -0500

    fix(app-router): force dynamic rendering for home page to support useSession on client only

--- app/page.js
+++ app/page.js
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { useSession } from 'next-auth/react';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
